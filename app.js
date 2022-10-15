4c778fbcee10695751fff460124a7931b62a17fe
BHARATHRAJ B <bharathraj1614@gmail.com>
2022-10-15 21:05:35 +0530

    last+2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb+srv://bharathraj1614:mugi2002@cluster0.br3ac.mongodb.net/myOrder');
+mongoose.connect('mongodb+srv://bharathraj1614:mugi2002@cluster0.br3ac.mongodb.net/myOrder?authSource=admin&compressors=zlib&retryWrites=true&w=majority&ssl=true');
 
 const ItemsSchema={
   name: String,
